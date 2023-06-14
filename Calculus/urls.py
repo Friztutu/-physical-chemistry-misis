3e67e6fcb36528c84329252dff21c14b5afdf041
Alex <erofeevs943@gmail.com>
2023-06-14 11:05:55 -1200

    (ADD and REFACTOR) Base.html - add href and Calculus templates view

--- Calculus/urls.py
+++ Calculus/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from Calculus.views import IndexView, Task1View
 
 app_name = 'Calculus'
 
 urlpatterns = [
-
+    path('', IndexView.as_view(), name='index'),
+    path('task1', Task1View.as_view(), name='task1'),
 ]
--- Calculus/views.py
+++ Calculus/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'Calculus/index.html'
+
+
+class Task1View(TemplateView):
+    template_name = 'Calculus/task1.html'
