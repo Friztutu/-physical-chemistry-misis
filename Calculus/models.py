74713c26acaad90ac99360d9ef6fff24e752b9b0
Alex <erofeevs@943@gmail.com>
2023-07-02 22:47:18 +0300

    (ADD) Models for first Task

--- Calculus/models.py
+++ Calculus/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Task1(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    power = models.PositiveSmallIntegerField(default=None)
+    amperage = models.PositiveSmallIntegerField(default=None)
+    scheme = models.CharField(max_length=32)
+    soil = models.CharField(max_length=64)
+    climate_zone = models.PositiveSmallIntegerField()
+
+
+class ResultTask1(models.Model):
+    diameter = models.FloatField()
+    vertical_length = models.FloatField()
+    num_accurate = models.FloatField()
+    scheme = models.CharField(max_length=32)
+    distance_between = models.FloatField()
+    section = models.FloatField()
+    length = models.FloatField()
+    depth = models.FloatField()
+    total_resistance = models.FloatField()
+    normative_resistance = models.FloatField()
