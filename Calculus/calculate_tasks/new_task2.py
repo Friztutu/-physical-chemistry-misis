39b583283f481b3eaea198624ebfa6a15355a5a1
Alex <erofeevs@943@gmail.com>
2023-07-10 22:22:58 +0300

    (ADD) Task2

--- Calculus/calculate_tasks/new_task2.py
+++ Calculus/calculate_tasks/new_task2.py
@@ -1,92 +1,136 @@
 import math
 
 
-# 72.528 * math.pow(x, -0.982)
-# 18.837 * math.pow(x, -0,952)
+class Task2Calculus:
+    SCHEMES = {1: 'звезда-звезда', 2: 'не звезда-звезда'}
+    MATERIALS = {1: 'алюминий', 2: 'медь'}
+    POWERS = {1: 40, 2: 63, 3: 400, 4: 630}
 
-# Ввод: P (мощность) # Соединение обмоток Y/Y # l, метры # Uф 220 вольт # медь/алюминий # D, метры # тип AB или П# номинальный ток
+    RESISTANCES = {40: (1.950, 0.562), 63: (1.240, 0.360), 400: (0.195, 0.056), 630: (0.129, 0.042)}
 
-def calculate(power, connection, length, voltage, metal, slice, distance, type, current):
-    impedance_table_1 = {25: 3.110, 40: 1.950, 63: 1.240, 100: 0.800, 160: 0.487,
-                         250: 0.312, 400: 0.195, 630: 0.129, 1000: 0.081, 1600: 0.054}
-    impedance_table_2 = {25: 0.906, 40: 0.562, 63: 0.360, 100: 0.266, 160: 0.141,
-                         250: 0.090, 400: 0.056, 630: 0.042, 1000: 0.029, 1600: 0.017}
+    In = {
+        0.5: {80: (5.24, 3.14), 120: (3.38, 2.03), 150: (3.38, 2.03), 160: (2.28, 1.37), 200: (2.28, 1.37),
+              250: (2.1, 1.26), 300: (1.77, 1.06)},
+        1: {80: (4.2, 2.52), 120: (2.91, 1.75), 150: (2.56, 1.54), 160: (2.24, 1.34), 200: (1.79, 1.07),
+            250: (1.60, 0.96), 300: (1.34, 0.8)},
+        1.5: {80: (3.48, 2.09), 120: (2.38, 1.43), 150: (2.08, 1.25), 160: (1.81, 1.09), 200: (1.45, 0.87),
+              250: (1.28, 0.77), 300: (1.08, 0.65)},
+        2: {80: (2.97, 1.78), 120: (2.04, 1.22), 150: (1.6, 0.98), 160: (1.54, 0.92), 200: (1.24, 0.74)}
+    }
 
-    if power not in impedance_table_1 and connection:
-        impedance = 72.528 * math.pow(power, -0.982)
-    elif power not in impedance_table_2:
-        impedance = 18.837 * math.pow(power, -0.952)
-    elif connection:
-        impedance = impedance_table_1[power]
-    else:
-        impedance = impedance_table_2[power]
+    def __init__(self, power_key: int, scheme_key: int, length: int, phase_material_id: int, phase_quantity: int,
+                 diameter: int, tok_power: int):
+        # User input
+        self.power = self.POWERS[power_key]
+        self.scheme = self.SCHEMES[scheme_key]
+        self.length = length
+        self.material = self.MATERIALS[phase_material_id]
+        self.material_quantity = phase_quantity
+        self.diameter = diameter
+        self.tok_power = tok_power
+        self.voltage = 220
 
-    if current < 100:
-        k = 1.4
-    else:
-        k = 1.25
+        # Tasks variables
+        self.Z = None
+        self.tok = None
+        self.current_density = None
+        self.Ri = None
+        self.Xi = None
+        self.Rn = None
+        self.Xn = None
+        self.Rf = None
+        self.Zn = None
+        self.Zf = None
+        self.d = None
+        self.Xp = None
+        self.Zp = None
+        self.Ii = None
+        self.Ln = None
 
-    short_current = k * current
+    def step_one(self):
+        if self.scheme == 'звезда-звезда':
+            self.Z = self.RESISTANCES[self.power][0]
+        else:
+            self.Z = self.RESISTANCES[self.power][1]
 
-    square_table = [80, 120, 150, 160, 200, 250, 300]
-    square_table_index = 6
-    null_current = short_current / square_table[square_table_index]
+    def step_two(self):
+        self.tok = round(self.tok_power * 1.4, 2)
 
-    while null_current <= 0.5 or null_current >= 2:
-        try:
-            square_table_index -= 1
-            null_current = short_current / square_table[square_table_index]
-        except IndexError:
-            return "Данных для расчёта нет в таблице, приносим свои извинения"
+    def step_three(self, Ln):
+        self.current_density = self.tok / Ln
 
-    res_table_05 = {80: (5.24, 3.14), 120: (3.66, 2.2), 150: (3.38, 2.03), 160: (2.8, 1.68),
-                    200: (2.28, 1.37), 250: (2.1, 1.26), 300: (1.77, 1.06)}
+        if not (0.5 <= self.current_density <= 2):
+            return True
 
-    res_table_1 = {80: (4.2, 2.52), 120: (2.91, 1.75), 150: (2.56, 1.54), 160: (2.24, 1.34),
-                   200: (1.79, 1.07), 250: (1.60, 0.96), 300: (1.34, 0.8)}
+        if self.current_density < 1:
+            self.current_density = 0.5
+        elif self.current_density < 1.5:
+            self.current_density = 1
+        else:
+            self.current_density = 1.5
 
-    res_table_15 = {80: (3.48, 2.09), 120: (2.38, 1.43), 150: (2.08, 1.25), 160: (1.81, 1.09),
-                    200: (1.45, 0.87), 250: (1.28, 0.77), 300: (1.08, 0.65)}
+        self.Ri = self.In[self.current_density][Ln][0]
+        self.Xi = self.In[self.current_density][Ln][1]
 
-    res_table_2 = {80: (2.97, 1.78), 120: (2.04, 1.22), 150: (1.6, 0.98), 160: (1.54, 0.92), 200: (1.24, 0.74)}
+    def step_four(self):
+        self.Rn = round(self.Ri * (self.length / 1000), 3)
+        self.Xn = round(self.Xi * (self.length / 1000), 3)
 
-    if 0.5 <= null_current < 1:
-        r1 = res_table_05[square_table[square_table_index]][0]
-        x1 = res_table_05[square_table[square_table_index]][1]
-        span = 1
-    elif 1 <= null_current < 1.5:
-        r1 = res_table_1[square_table[square_table_index]][0]
-        x1 = res_table_1[square_table[square_table_index]][1]
-        span = 2
-    elif 1.5 <= null_current < 2:
-        r1 = res_table_15[square_table[square_table_index]][0]
-        x1 = res_table_15[square_table[square_table_index]][1]
-        span = 3
-    else:
-        r1 = res_table_2[square_table[square_table_index]][0]
-        x1 = res_table_2[square_table[square_table_index]][1]
-        span = 4
+    def step_five(self):
+        if self.material == 'алюминий':
+            self.Rf = round((0.028 * self.length) / self.material_quantity, 3)
+        else:
+            self.Rf = round((0.018 * self.length) / self.material_quantity, 3)
 
-    check_dictionary = {1: res_table_05, 2: res_table_1, 3: res_table_15, 4: res_table_2}
+    def step_six(self):
+        self.Zn = round(math.sqrt(math.pow(self.Rn, 2) + math.pow(self.Xn, 2)), 3)
+        self.Zf = self.Rf
 
-    null_active_resistance = r1 * length
-    null_reactive_resistance = x1 * length
+    def step_seven(self):
+        return self.Zn <= 2 * self.Zf
 
-    if metal:
-        resistivity = 0.018
-    else:
-        resistivity = 0.028
+    def step_eight(self):
+        odd = self.material_quantity / 3.14
+        self.d = round(2 * math.sqrt(odd), 2)
+        self.Xp = round(0.1256 * (self.length / 1000) * math.log(2 * self.diameter / (self.d * 10 ** -6)), 3)
 
-    phase_impedance = resistivity * length / slice
-    null_impedance = math.sqrt(math.pow(null_active_resistance, 2) + math.pow(null_reactive_resistance, 2))
-    diameter = 2 * math.sqrt(slice/math.pi)
-    loop_reactive_resistance = 0.1256 * length * math.log1p(2 * distance / diameter)
+    def step_nine(self):
+        self.Zp = round(math.sqrt(math.pow(self.Rf + self.Rn, 2) + math.pow(0 + self.Xn + self.Xp, 2)), 2)
 
-    complete_impedance = math.sqrt(math.pow(null_active_resistance + phase_impedance, 2)
-                                   + math.pow(null_reactive_resistance + loop_reactive_resistance, 2))
+    def step_ten(self):
+        self.Ii = round(220 / (self.Z / 3 + self.Zp), 1)
 
-    calculated_current = voltage / (impedance/3 + complete_impedance)
+    def step_eleven(self):
+        return self.Ii >= self.tok
 
-    return calculated_current
+    def __call__(self, *args, **kwargs):
+        keys = (80, 120, 150, 160, 200, 250, 300)
+
+        self.step_one()
+        self.step_two()
+
+        for self.Ln in keys:
+
+            if self.step_three(self.Ln):
+                continue
+
+            self.step_four()
+            self.step_five()
+            self.step_six()
+
+            if not self.step_seven():
+                continue
+
+            self.step_eight()
+            self.step_nine()
+            self.step_ten()
+
+            if self.step_eleven():
+                break
+
+        return self.Ln
 
 
+if __name__ == '__main__':
+    task2 = Task2Calculus(2, 1, 250, 2, 15, 0.3, 40)
+    print(task2())
